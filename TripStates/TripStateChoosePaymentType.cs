64e914bd51eba5ec38491767bddfd127662e3f03
Nelso Villalobos <nelso.lobos13@gmail.com>
2023-04-25 14:31:06 -0400

    Finished Up TripStateChoosePaymentType.cs.

--- TripStates/TripStateChoosePaymentType.cs
+++ TripStates/TripStateChoosePaymentType.cs
@@ -4,6 +4,28 @@ public class TripStateChoosePaymentType: TripState
 {
     public override Status Execute(TripContext context)
     {
-        throw new NotImplementedException();
+        do
+        {
+            Console.WriteLine("***************************");
+            Console.WriteLine("*** Choose Payment Type ***");
+            Console.WriteLine("***************************\n");
+            Console.WriteLine("Are You Paying Cash or Credit?\n" +
+                              "Enter \"cash\" for cash or \"credit\" for credit or \"quit\" to quit");
+            var answer = Console.ReadLine();
+            switch (answer)
+            {
+                case "cash":
+                    context.State = new TripStatePayCash();
+                    return Status.PayCash;
+                case "credit":
+                    context.State = new TripStatePayCredit();
+                    return Status.PayCredit;
+                case "quit":
+                    return Status.ChoosePayment;
+                default:
+                    Console.WriteLine("Wrong input, Try Again");
+                    break;
+            }
+        } while (true);
     }
 }
