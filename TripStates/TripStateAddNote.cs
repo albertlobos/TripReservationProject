5bf792f5a4aea7c60ad57a511a5023b91ea6f0f0
Nelso Villalobos <nelso.lobos13@gmail.com>
2023-04-25 16:04:13 -0400

    Set Up TripStateAddNote.cs.

--- TripStates/TripStateAddNote.cs
+++ TripStates/TripStateAddNote.cs
@@ -4,6 +4,39 @@ public class TripStateAddNote: TripState
 {
     public override Status Execute(TripContext context)
     {
-        throw new NotImplementedException();
+        return AddNote(context);
+    }
+
+    private static Status AddNote(TripContext context)
+    {
+        Console.WriteLine("*************************");
+        Console.WriteLine("***   Create Note   ***");
+        Console.WriteLine("*************************\n");
+        do
+        {
+            Console.WriteLine("Write the note you would like to add to the trip. or type \"quit\" to simply quit");
+            var note = Console.ReadLine();
+            if (note == "quit")
+            {
+                return Status.AddNote;
+            }
+            else
+            {
+                Console.WriteLine("Your note says:  \n" + note);
+                Console.WriteLine("Do you want to continue adding this note? Y/N/quit");
+                note = Console.ReadLine();
+
+                switch (note)
+                {
+                    case "quit":
+                        return Status.AddNote;
+                    case "Y":
+                        context.State = new TripStateComplete();
+                        return Status.Complete;
+                    case "N":
+                        break;
+                }
+            }
+        } while (true);
     }
 }
