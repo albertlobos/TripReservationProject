6f8b3b18549de1fed2bf847ac651fb8d67e141de
Nelso Villalobos <nelso.lobos13@gmail.com>
2023-05-01 12:54:11 -0400

    Extra comments

--- CheckPayment.cs
+++ CheckPayment.cs
@@ -1,5 +1,8 @@
 namespace TripReservation;
 
+/*
+ * This is a child class that extends from Payment. It will be attached to the client's trip
+ */
 public class CheckPayment : Payment
 {
     public CheckPayment(decimal amount, int checkNumber) : base(amount)
